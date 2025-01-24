0879125722af178f56afdfb36b6e2eb2a1239e32
Kasim Pasaoglu <kasimpasaoglu@windowslive.com>
2025-01-24 12:24:50 +0300

    test

--- Infrastructure/LoginValidator.cs
+++ Infrastructure/LoginValidator.cs
@@ -6,6 +6,7 @@ public class LoginValidator : AbstractValidator<Login>
     {
         RuleFor(login => login.PhoneNumber)
             .NotEmpty().WithMessage("Bu alan doldurulmalidir")
-            .Must(phone => phone.StartsWith("05")).Matches(@"^\d{11}$").WithMessage("Telefon numarasi hatali, lutfen tekrar deneyiniz");
+            .Must(phone => phone.StartsWith("05")).WithMessage("Telefon numaraniz '05' ile baslamalidir")
+            .Matches(@"^\d{11}$").WithMessage("Telefon numarasi hatali, lutfen tekrar deneyiniz");
     }
 }
--- Layers/Controllers/LoginController.cs
+++ Layers/Controllers/LoginController.cs
@@ -2,17 +2,31 @@ using Microsoft.AspNetCore.Mvc;
 
 public class LoginController : Controller
 {
-
-    public LoginController()
+    private readonly ILoginService _loginService;
+    public LoginController(ILoginService loginService)
     {
-
+        _loginService = loginService;
     }
-
     public IActionResult Index()
     {
         return View();
     }
+
+    [HttpPost]
+    public IActionResult Index(Login model)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(model);
+        }
+        return RedirectToAction("RequestDetail");
+    }
     public IActionResult LegalNotice()
+    {
+        return View();
+    }
+
+    public IActionResult RequestDetail()
     {
         return View();
     }
