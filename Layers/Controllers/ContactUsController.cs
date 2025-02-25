d376fe1de7ff149b759564cff44237c6f31f7141
Kasim Pasaoglu <kasimpasaoglu@windowslive.com>
2025-02-25 13:02:42 +0300

    mail gonderimi

--- Layers/Controllers/ContactUsController.cs
+++ Layers/Controllers/ContactUsController.cs
@@ -1,3 +1,4 @@
+using System.Net.Mail;
 using System.Threading.Tasks;
 using FluentValidation;
 using Microsoft.AspNetCore.Mvc;
@@ -7,11 +8,13 @@ public class ContactUsController : Controller
 {
     private readonly IValidator<ContactFormVM> _validator;
     private readonly IContactUsService _contactUsService;
+    private readonly ILoginService _loginService;
 
-    public ContactUsController(IValidator<ContactFormVM> validator, IContactUsService contactUsService)
+    public ContactUsController(IValidator<ContactFormVM> validator, IContactUsService contactUsService, ILoginService loginService)
     {
         _validator = validator;
         _contactUsService = contactUsService;
+        _loginService = loginService;
     }
 
     public IActionResult Index()
@@ -34,6 +37,7 @@ public class ContactUsController : Controller
         var result = await _contactUsService.Put(user.UserId, model.Message);
         if (result > 0)
         {
+            await SendEmail(model, user);
             return RedirectToAction("Success");
         }
         return View(model);
@@ -42,5 +46,47 @@ public class ContactUsController : Controller
     public IActionResult Success()
     {
         return View();
+    }
+
+    private async Task SendEmail(ContactFormVM model, UserVM user)
+    {
+
+        var business = await _loginService.FindBusinessById(user.BusinessId); // burda bir hata yok. 
+
+        // template ayri bir dosayada almak yerine direk burda olusturulabilir
+        string template = $@" 
+            <html>
+            <body>
+                <h2>Yeni Mesaj</h2>
+                <p><strong>Ad:</strong> {user.Name}</p>
+                <p><strong>Soyad:</strong> {user.Surname}</p>
+                <p><strong>Şirket:</strong> {business.BusinessName}</p>
+                <p><strong>Telefon:</strong> {user.PhoneNumber}</p>
+                <p><strong>Mesaj:</strong> {model.Message}</p>
+            </body>
+            </html>";
+
+        // Send Email 
+        using (SmtpClient cli = new SmtpClient("smtp.gmail.com", 587)) //gmail protu 587 olmalu
+        {
+
+            cli.EnableSsl = true;
+            cli.Credentials = new System.Net.NetworkCredential("GMAIL_ADDRESS", "GMAIL_APP_PASSWORD"); // gonderici hesaba ait giris bilgileri env olarak atanmali
+            MailMessage message = new()
+            {
+                Subject = $"Esnafa Gelir {user.Name} kullanıcısı mesaj gönderdi",
+                Body = template,
+                From = new MailAddress("developer@esnafagelir.com"),
+                IsBodyHtml = true,
+            };
+
+            // alicilar
+            message.To.Add("emrahelis@gmail.com");
+            message.To.Add("bilgi@esnafagelir.com");
+            message.To.Add("orhunozbalkan@gmail.com");
+            message.To.Add("ismerd73@hotmail.com");
+
+            await cli.SendMailAsync(message); // send yerine asenkron olani tercih edilmeli
+        }
     }
 }
